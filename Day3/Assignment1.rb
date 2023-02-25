b96e8c7db83eb2122b2048eda5ad5ba83daa6780
KrupaKhant24 <117979907+KrupaKhant24@users.noreply.github.com>
2023-02-25 23:17:21 +0530

    Update Assignment1.rb

--- Day3/Assignment1.rb
+++ Day3/Assignment1.rb
@@ -1,16 +1,16 @@
 EXPECTED_MINUTES_IN_OVEN=40
-
+LAYER_TIME = 2
 class Lasanga 
 	puts "expected minutes is #{EXPECTED_MINUTES_IN_OVEN}"
  
-	def remaining_minutes_in_oven(t)
-	  result=EXPECTED_MINUTES_IN_OVEN-t
+	def remaining_minutes_in_oven(time)
+	  result=EXPECTED_MINUTES_IN_OVEN-time
 	  puts "remaining #{result}"
 	  return result
 	end
 
 	def preparation_time_in_minutes(layer)
-	  totallayer=layer * 2
+	  totallayer=layer * LAYER_TIME
 	  puts "Layer #{totallayer}"
 	  return totallayer
 	end
